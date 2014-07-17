370a3acae3723727565b7f224025cfb288f3ad32
sergio ivan lopez monzon <sergioivan154@gmail.com>
2014-07-16 19:51:19 -0500

    clase operación

---
+++ iOScalculator/iOScalculator/clase.h
@@ -0,0 +1,13 @@
+//
+//  clase.h
+//  iOScalculator
+//
+//  Created by sergio ivan lopez monzon on 16/07/14.
+//  Copyright (c) 2014 Emmanuel Valentin Granados Lopez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface clase : NSObject
+-(NSInteger)sumarValor1:(NSInteger) conValor2:(NSInteger);
+@end
